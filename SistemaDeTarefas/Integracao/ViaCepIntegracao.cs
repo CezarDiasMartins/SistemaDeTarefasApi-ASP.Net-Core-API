77224b27f6db223886f94af615906e85d96087e3
CezarDiasMartins <cezardiasmartins06@hotmail.com>
2023-11-25 18:10:29 -0400

    primeiro commit

---
+++ SistemaDeTarefas/Integracao/ViaCepIntegracao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using SistemaDeTarefas.Integracao.Interfaces;
+using SistemaDeTarefas.Integracao.Refit;
+using SistemaDeTarefas.Integracao.Response;
+
+namespace SistemaDeTarefas.Integracao
+{
+    public class ViaCepIntegracao : IViaCepIntegracao
+    {
+        private readonly IViaCepIntegracaoRefit _viaCepIntegracaoRefit;
+
+        public ViaCepIntegracao(IViaCepIntegracaoRefit viaCepIntegracaoRefit)
+        {
+            _viaCepIntegracaoRefit = viaCepIntegracaoRefit;
+        }
+
+        public async Task<ViaCepResponse> OberDadosViaCep(string cep)
+        {
+            var responseData = await _viaCepIntegracaoRefit.ObterDadosViaCep(cep);
+
+            if (responseData != null && responseData.IsSuccessStatusCode)
+            {
+                return responseData.Content;
+            }
+
+            return null;
+        }
+    }
+}
